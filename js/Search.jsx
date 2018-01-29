503017c8e70eb3c0f9cbcb2c8b3378d2a37d378c
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-29 15:42:15 +0200

    Loading JSON data -  using map to iterate and display title

--- js/Search.jsx
+++ js/Search.jsx
@@ -7,7 +7,9 @@ class Search extends Component {
   render() {
     return (
       <div className="search">
-        <pre><code>{JSON.stringify(preload, null, 4)}</code></pre>
+        {preload.shows.map(
+          (show) => <h3>{show.title}</h3>)
+        }
       </div>
     );
   }
