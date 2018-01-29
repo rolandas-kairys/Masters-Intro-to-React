c22bf8993c02ca36721ead9e35e029938860d628
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-29 16:15:45 +0200

    Making ShowCard Component - extending data and markup for cards

--- js/Search.jsx
+++ js/Search.jsx
@@ -8,8 +8,18 @@ class Search extends Component {
     return (
       <div className="search">
         {preload.shows.map(
-          (show) => <h3>{show.title}</h3>)
-        }
+          (show) =>
+            <div className="show-card">
+              <img
+                src={`/public/img/posters/${show.poster}`}
+                alt={`${show.title} Show Poster`} />
+              <div>
+                <h3>{show.title}</h3>
+                <h4> ({show.year} )</h4>
+                <p> {show.description} </p>
+              </div>
+            </div>
+        )}
       </div>
     );
   }
