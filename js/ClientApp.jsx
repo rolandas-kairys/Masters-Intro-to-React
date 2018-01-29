224794f642d53bc6698afa04a1789561826ab3f9
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-29 13:43:32 +0200

    start application

---
+++ js/ClientApp.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { HashRouter, Route } from 'react-router-dom'
+import Landing from './Landing';
+
+const App = () => (
+  <HashRouter>
+    <div className="app">
+      <Route exact path="/" component={Landing} />
+    </div>
+  </HashRouter>
+);
+
+render(<App />, document.getElementById('app'));
