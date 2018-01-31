f180fcbb673296ba7ec10a37db04a8930d15e348
rolandas-kairys <rolandas.kairys@gmail.com>
2018-01-31 13:21:22 +0200

    Setting Up HMR - moving App to a separate file

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,22 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Landing from './Landing';
-import Search from './Search';
+import App from './App';
 
 
-const FourOhFour = () => <h1>404 - Page Not Found</h1>
 
-const App = () => (
-  <BrowserRouter>
-    <div className="app">
-      <Switch>
-        <Route exact path="/" component={Landing} />
-        <Route path="/search" component={Search} />
-        <Route component={FourOhFour} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
 
 render(<App />, document.getElementById('app'));
