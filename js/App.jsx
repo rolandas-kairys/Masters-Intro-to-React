a3798b56c435e18736d560a957b2ef87063febd4
rolandas-kairys <rolandas.kairys@gmail.com>
2018-02-01 20:51:38 +0200

    URL Parameters - Details included in a Route

--- js/App.jsx
+++ js/App.jsx
@@ -5,6 +5,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Landing from './Landing';
 import Search from './Search';
+import Details from './Details';
 
 
 
@@ -16,6 +17,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
+        <Route path="/details" component={Details} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
