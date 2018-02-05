10ca9ae9da6b8c17dde54501b32f287d94bb643e
rolandas-kairys <rolandas.kairys@gmail.com>
2018-02-05 15:22:04 +0200

    Finishing Details Component - using HTML markup to display data

--- js/Details.jsx
+++ js/Details.jsx
@@ -7,7 +7,23 @@ const Details = (props: { show: Show }) => {
   const { title, description, year, poster, trailer } = props.show;
   return (
     <div className="details" >
-      <pre><code>  {JSON.stringify(props, null, 4)}</code> </pre>
+      <header>
+        <h1>s-video</h1>
+      </header>
+      <section>
+        <h1>{ title }</h1>
+        <h2>({ year })</h2>
+        <img src={ `/public/img/posters/${poster}` } alt={ `Poster for ${title}` } />
+        <p>{ description }</p>
+      </section>
+      <div>
+        <iframe
+          src={ `https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0amp;showinfo=0` }
+          frameBorder="0"
+          allowFullScreen
+          title={ `Trailer for ${title}` }
+        ></iframe>
+      </div>
     </div>
   );
 }
