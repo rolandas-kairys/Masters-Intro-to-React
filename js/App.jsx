b85979bcc0ed7ae397e2fa4f1b90e3322f037985
rolandas-kairys <rolandas.kairys@gmail.com>
2018-02-05 12:44:50 +0200

    URL Parameters - rendering parameter "stuff"

--- js/App.jsx
+++ js/App.jsx
@@ -18,7 +18,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
-        <Route path="/details/:id" component={() => <Details />} />
+        <Route path="/details/:id" component={() => <Details stuff="cool" />} />
         <Route component={FourOhFour} />
       </Switch>
     </div>
