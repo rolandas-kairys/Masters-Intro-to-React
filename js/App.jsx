0b85aad3c217cdbd6c4e70c32edc10b20df5338d
rolandas-kairys <rolandas.kairys@gmail.com>
2018-02-05 12:22:02 +0200

    URL Parameters - importing data to App

--- js/App.jsx
+++ js/App.jsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
+import preload from '../data.json';
 
 
 
