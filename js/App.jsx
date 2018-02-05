ed5822fc58f4119a492605fd449affd81de2a5ee
rolandas-kairys <rolandas.kairys@gmail.com>
2018-02-05 13:54:43 +0200

    URL Parameters - Flow type import, props type validation with Match

--- js/App.jsx
+++ js/App.jsx
@@ -3,6 +3,7 @@
 
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import type { Match } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
 import Details from './Details';
@@ -18,7 +19,7 @@ const App = () => (
       <Switch>
         <Route exact path="/" component={Landing} />
         <Route path="/search" component={Search} />
-        <Route path="/details/:id" component={(props) =>
+        <Route path="/details/:id" component={(props: { match: Match }) =>
             {
               const selectedShow = preload.shows.find( (show) => props.match.params.id === show.imdbID );
               return <Details show={   selectedShow  } />
