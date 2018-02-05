b32e504814d81bf5d46e6a033512e21bef28e9c0
rolandas-kairys <rolandas.kairys@gmail.com>
2018-02-05 15:02:25 +0200

    Finishing Details Component - defining & destructuring props

--- js/Details.jsx
+++ js/Details.jsx
@@ -4,6 +4,7 @@
 import React from 'react';
 
 const Details = (props) => {
+  const { title, description, year, poster, trailer } = props.show;
   return (
     <div className="details" >
       <pre><code>  {JSON.stringify(props, null, 4)}</code> </pre>
