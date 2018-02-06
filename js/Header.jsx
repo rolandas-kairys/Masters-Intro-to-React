bb39bcd6e7aec4fbbeaa0e39a6385e1ad99d3d2e
rolandas-kairys <rolandas.kairys@gmail.com>
2018-02-06 14:06:29 +0200

    Creating Header - restructuring component

--- js/Header.jsx
+++ js/Header.jsx
@@ -5,12 +5,15 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 
-const Header = () => (
-  <header>
-    <h1>
-      <Link to="/"> s-video </Link>
-    </h1>
-  </header>
-);
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        <Link to="/"> s-video </Link>
+      </h1>
+    </header>
+  );
+};
+
 
 export default Header;
