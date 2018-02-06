5b946a1a9ea1aee295dea261d77b8989e4bc7abb
rolandas-kairys <rolandas.kairys@gmail.com>
2018-02-06 13:43:30 +0200

    Creating Header - importing Header component

--- js/Details.jsx
+++ js/Details.jsx
@@ -2,6 +2,9 @@
 
 
 import React from 'react';
+import Header from './Header';
+
+
 
 const Details = (props: { show: Show }) => {
   const { title, description, year, poster, trailer } = props.show;
@@ -11,17 +14,17 @@ const Details = (props: { show: Show }) => {
         <h1>s-video</h1>
       </header>
       <section>
-        <h1>{ title }</h1>
-        <h2>({ year })</h2>
-        <img src={ `/public/img/posters/${poster}` } alt={ `Poster for ${title}` } />
-        <p>{ description }</p>
+        <h1>{title}</h1>
+        <h2>({year})</h2>
+        <img src={`/public/img/posters/${poster}`} alt={`Poster for ${title}`} />
+        <p>{description}</p>
       </section>
       <div>
         <iframe
-          src={ `https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0amp;showinfo=0` }
+          src={`https://www.youtube-nocookie.com/embed/${trailer}?rel=0&amp;controls=0amp;showinfo=0`}
           frameBorder="0"
           allowFullScreen
-          title={ `Trailer for ${title}` }
+          title={`Trailer for ${title}`}
         ></iframe>
       </div>
     </div>
