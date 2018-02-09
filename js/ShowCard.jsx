a9a865fe4eb3e43f50e4991ea4dc2ded869b681f
rolandas-kairys <rolandas.kairys@gmail.com>
2018-02-09 13:01:40 +0200

    ShowCard Link - using Link inside Styled component & correcting style attr

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -8,13 +8,15 @@ import { Link } from 'react-router-dom';
 
 
 const color = "#333";
-const Wrapper = styled.div`
+const Wrapper = styled(Link) `
   width: 32%;
   border: 2px solid ${color};
   border-radius: 25px;
   margin-bottom: 25px;
   padding-right: 10px;
   overflow: hidden;
+  color: black;
+  text-decoration: none;
 `;
 
 const Image = styled.img`
@@ -26,17 +28,15 @@ const Image = styled.img`
 
 const ShowCard = (props: { show: { poster: string, title: string, year: string, description: string, imdbID: string } }) => {
   return (
-    <Wrapper className="show-card">
-      <Link to={`/details/${props.show.imdbID}`}>
-        <Image
-          src={`/public/img/posters/${props.show.poster}`}
-          alt={`${props.show.title} Show Poster`} />
-        <div>
-          <h3>{props.show.title}</h3>
-          <h4> ({props.show.year} )</h4>
-          <p> {props.show.description} </p>
-        </div>
-      </Link>
+    <Wrapper to={`/details/${props.show.imdbID}`} className="show-card">
+      <Image
+        src={`/public/img/posters/${props.show.poster}`}
+        alt={`${props.show.title} Show Poster`} />
+      <div>
+        <h3>{props.show.title}</h3>
+        <h4> ({props.show.year} )</h4>
+        <p> {props.show.description} </p>
+      </div>
     </Wrapper>
   );
 }
